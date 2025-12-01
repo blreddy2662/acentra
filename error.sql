b0d4a1eccd273e64f30e968610b0208b6f8e7e72
blreddy2662 <blreddy2662@gmail.com>
2025-12-01 20:28:14 +0530

    Update error.sql

--- error.sql
+++ error.sql
@@ -1,4 +1,10 @@
-SQL Error [42601]: ERROR: syntax error at or near "DEFAULT" in context "IN p_batch_size integer DEFAULT", at line 23, column 29
-  Position: 481
-
-Error position: line: 12 pos: 480
+SQL Error [XX000]: ERROR: Result size exceeds LISTAGG limit
+  Detail: 
+  -----------------------------------------------
+  error:  Result size exceeds LISTAGG limit
+  code:      8001
+  context:   LISTAGG limit: 16000000
+  query:     179939852
+  location:  string_ops.cpp:144
+  process:   query0_117_179939852 [pid=5701]
+  -----------------------------------------------
